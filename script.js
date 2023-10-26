6cff3d79cc9ddb5a92769f296b607b8f3a11de3f
Gabrielyn Strujak <107216619+strujakgabrielyn@users.noreply.github.com>
2023-10-26 17:10:19 -0300

    development index, css and script.

---
+++ script.js
@@ -0,0 +1,8 @@
+ScrollReveal().reveal('.height-100', {
+    delay: 300,        // Atraso em milissegundos
+    distance: '20px',  // Distância de deslocamento
+    origin: 'bottom',  // Origem da animação
+    duration: 800,     // Duração da animação em milissegundos
+    easing: 'cubic-bezier(0.5, 0, 0, 1)', // Função de aceleração
+    reset: true        // Rode a animação a cada vez que o elemento entrar na visualização
+});
